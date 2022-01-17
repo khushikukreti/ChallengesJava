6261cfb051030b741b16229886c113e1dee19591
khushi23 <khushikukreti20@gmail.com>
2022-01-17 15:02:54 +0530

    challenges
    linkedin java8 challenges completed

---
+++ SimpleCalculator.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+/**
+ * SimpleCalculator
+ */
+public class SimpleCalculator {
+
+    public static void main(String[] args) {
+        Scanner scan= new Scanner(System.in);
+        System.out.println("Enter a numeric value: ");
+        String input1= scan.nextLine();
+        double d1=Double.parseDouble(input1);
+
+        System.out.println("Enter a numeric value: ");
+        String input2= scan.nextLine();
+        double d2=Double.parseDouble(input2);
+
+        double res=d1+d2;
+
+        System.out.println("The answer is "+ res);
+    }
+}
